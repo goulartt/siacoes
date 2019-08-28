0c2e7ef0aef1b74357c3c467efbd5c35d31aa1be
João Goulart <jv.goulart.almeida@hotmail.com>
2019-08-28 20:30:46 -0300

    adição de mais 5 classes usando lombok e verificando qtd de linhas

--- src/main/java/br/edu/utfpr/dv/siacoes/model/ActivitySubmissionItem.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/ActivitySubmissionItem.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 
 import br.edu.utfpr.dv.siacoes.model.ActivitySubmission.ActivityFeedback;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class ActivitySubmissionItem implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -13,36 +18,5 @@ public class ActivitySubmissionItem implements Serializable {
 	private ActivitySubmission submission;
 	private ActivityFeedback feedback;
 	
-	public ActivitySubmissionItem(){
-		this.setIdActivitySubmissionItem(0);
-		this.setActivity(new Activity());
-		this.setSubmission(new ActivitySubmission());
-		this.setFeedback(ActivityFeedback.NONE);
-	}
-	
-	public int getIdActivitySubmissionItem() {
-		return idActivitySubmissionItem;
-	}
-	public void setIdActivitySubmissionItem(int idActivitySubmissionItem) {
-		this.idActivitySubmissionItem = idActivitySubmissionItem;
-	}
-	public Activity getActivity() {
-		return activity;
-	}
-	public void setActivity(Activity activity) {
-		this.activity = activity;
-	}
-	public ActivitySubmission getSubmission() {
-		return submission;
-	}
-	public void setSubmission(ActivitySubmission submission) {
-		this.submission = submission;
-	}
-	public ActivityFeedback getFeedback() {
-		return feedback;
-	}
-	public void setFeedback(ActivityFeedback feedback) {
-		this.feedback = feedback;
-	}
 	
 }
