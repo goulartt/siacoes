8ca0d4bfd0a4e53ced23b141e9a20a53e68e6af9
João Goulart <jv.goulart.almeida@hotmail.com>
2019-08-28 19:32:14 -0300

    5 classes alteradas para utilização da biblioteca lombok

--- src/main/java/br/edu/utfpr/dv/siacoes/model/ActivityGroupStatus.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/ActivityGroupStatus.java
@@ -2,6 +2,11 @@ package br.edu.utfpr.dv.siacoes.model;
 
 import java.io.Serializable;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class ActivityGroupStatus implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -9,22 +14,4 @@ public class ActivityGroupStatus implements Serializable {
 	private ActivityGroup group;
 	private double averageScore;
 	
-	public ActivityGroupStatus() {
-		this.setGroup(new ActivityGroup());
-		this.setAverageScore(0);
-	}
-	
-	public ActivityGroup getGroup() {
-		return group;
-	}
-	public void setGroup(ActivityGroup group) {
-		this.group = group;
-	}
-	public double getAverageScore() {
-		return averageScore;
-	}
-	public void setAverageScore(double averageScore) {
-		this.averageScore = averageScore;
-	}
-
 }
--- src/main/java/br/edu/utfpr/dv/siacoes/model/ActivitySubmission.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/ActivitySubmission.java
@@ -5,34 +5,40 @@ import java.util.Date;
 
 import br.edu.utfpr.dv.siacoes.util.DateUtils;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class ActivitySubmission implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
-	public enum ActivityFeedback{
+
+	public enum ActivityFeedback {
 		NONE(0), APPROVED(1), DISAPPROVED(2);
-		
-		private final int value; 
-		ActivityFeedback(int value){ 
-			this.value = value; 
+
+		private final int value;
+
+		ActivityFeedback(int value) {
+			this.value = value;
 		}
-		
-		public int getValue(){ 
+
+		public int getValue() {
 			return value;
 		}
-		
-		public static ActivityFeedback valueOf(int value){
-			for(ActivityFeedback p : ActivityFeedback.values()){
-				if(p.getValue() == value){
+
+		public static ActivityFeedback valueOf(int value) {
+			for (ActivityFeedback p : ActivityFeedback.values()) {
+				if (p.getValue() == value) {
 					return p;
 				}
 			}
-			
+
 			return null;
 		}
-		
-		public String toString(){
-			switch(this){
+
+		public String toString() {
+			switch (this) {
 			case NONE:
 				return "Nenhum";
 			case APPROVED:
@@ -44,7 +50,7 @@ public class ActivitySubmission implements Serializable {
 			}
 		}
 	}
-	
+
 	private int idActivitySubmission;
 	private User student;
 	private User feedbackUser;
@@ -62,145 +68,24 @@ public class ActivitySubmission implements Serializable {
 	private String description;
 	private int stage;
 	private String feedbackReason;
-	
-	public ActivitySubmission(){
-		this.setIdActivitySubmission(0);
-		this.setStudent(new User());
-		this.setFeedbackUser(new User());
-		this.setDepartment(new Department());
-		this.setActivity(new Activity());
-		this.setSemester(DateUtils.getSemester());
-		this.setYear(DateUtils.getYear());
-		this.setSubmissionDate(new Date());
-		this.setFile(null);
-		this.setAmount(0);
-		this.setFeedback(ActivityFeedback.NONE);
-		this.setFeedbackDate(null);
-		this.setValidatedAmount(0);
-		this.setComments("");
-		this.setDescription("");
-		this.setStage(0);
-		this.setFeedbackReason("");
-	}
-	
-	public int getIdActivitySubmission() {
-		return idActivitySubmission;
-	}
-	public void setIdActivitySubmission(int idActivitySubmission) {
-		this.idActivitySubmission = idActivitySubmission;
-	}
-	public User getStudent() {
-		return student;
-	}
-	public void setStudent(User student) {
-		this.student = student;
-	}
-	public User getFeedbackUser() {
-		return feedbackUser;
-	}
-	public void setFeedbackUser(User feedbackUser) {
-		this.feedbackUser = feedbackUser;
-	}
-	public Department getDepartment() {
-		return department;
-	}
-	public void setDepartment(Department department) {
-		this.department = department;
-	}
-	public Activity getActivity(){
-		return activity;
-	}
-	public void setActivity(Activity activity){
-		this.activity = activity;
-	}
-	public int getSemester() {
-		return semester;
-	}
-	public void setSemester(int semester) {
-		this.semester = semester;
-	}
-	public int getYear() {
-		return year;
-	}
-	public void setYear(int year) {
-		this.year = year;
-	}
-	public Date getSubmissionDate() {
-		return submissionDate;
-	}
-	public void setSubmissionDate(Date submissionDate) {
-		this.submissionDate = submissionDate;
-	}
-	public byte[] getFile() {
-		return file;
-	}
-	public void setFile(byte[] file) {
-		this.file = file;
-	}
-	public double getAmount() {
-		return amount;
-	}
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-	public ActivityFeedback getFeedback(){
-		return feedback;
-	}
-	public void setFeedback(ActivityFeedback feedback){
-		this.feedback = feedback;
-	}
-	public Date getFeedbackDate(){
-		return feedbackDate;
-	}
-	public void setFeedbackDate(Date feedbackDate){
-		this.feedbackDate = feedbackDate;
-	}
-	public double getValidatedAmount(){
-		return validatedAmount;
-	}
-	public void setValidatedAmount(double validatedAmount){
-		this.validatedAmount = validatedAmount;
-	}
-	public String getComments() {
-		return comments;
-	}
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
+
 	public double getScore() {
 		double score = 0;
-		
-		if(this.getFeedback() == ActivityFeedback.APPROVED) {
-			if(this.getActivity().getUnit().isFillAmount()) {
+
+		if (this.getFeedback() == ActivityFeedback.APPROVED) {
+			if (this.getActivity().getUnit().isFillAmount()) {
 				score = this.getActivity().getScore() * this.getValidatedAmount();
 			} else {
 				score = this.getActivity().getScore();
 			}
-			
-			if((this.getActivity().getMaximumInSemester() > 0) && (score > this.getActivity().getMaximumInSemester())) {
+
+			if ((this.getActivity().getMaximumInSemester() > 0)
+					&& (score > this.getActivity().getMaximumInSemester())) {
 				score = this.getActivity().getMaximumInSemester();
 			}
 		}
-		
+
 		return score;
-	}
-	public int getStage() {
-		return stage;
-	}
-	public void setStage(int stage) {
-		this.stage = stage;
-	}
-	public String getFeedbackReason() {
-		return feedbackReason;
-	}
-	public void setFeedbackReason(String feedbackReason) {
-		this.feedbackReason = feedbackReason;
 	}
 
 }
